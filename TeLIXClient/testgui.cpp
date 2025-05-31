c10c8017fed5ef3c56568bfc6e37edf6b5c00d6e
James <thejamesash1@gmail.com>
2025-05-31 14:13:10 -0400

    Add some testing details to test gui

--- TeLIXClient/testgui.cpp
+++ TeLIXClient/testgui.cpp
@@ -1,12 +1,30 @@
 #include "overlay.h"
 #include "imgui/imgui.h"
 
+#include "iracing.h"
+
 void overlay::draw_test_gui() {
 	ImGui::SetNextWindowSize(ImVec2(600, 400));
 
+	float speed = ir_Speed.getFloat();
+	float brake = ir_Brake.getFloat();
+	float throttle = ir_Throttle.getFloat();
+	float steeringAngle = ir_SteeringWheelAngle.getFloat();
+
+	float gear = ir_Gear.getInt();
+	float rpm = ir_RPM.getFloat();
+
+
 	ImGui::Begin("gui", nullptr, ImGuiWindowFlags_NoDecoration);
 	{
-		ImGui::Text("overlay test");
+		
+		ImGui::Text("Speed: %f m/s", speed);
+		ImGui::Text("Brake: %f", brake);
+		ImGui::Text("Throttle: %f", throttle);
+		ImGui::Text("Steering Angle: %f degrees", steeringAngle);
+		ImGui::Text("Gear: %d", gear);
+		ImGui::Text("RPM: %f r/m", rpm);
+
 	}
 	ImGui::End();
 }
